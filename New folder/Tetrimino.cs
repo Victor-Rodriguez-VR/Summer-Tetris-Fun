5000089a699ba252b49bf904f5ef89341d5055b9
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-08 21:07:25 -0700

    Cleaned up the main Repo. Also updates.
    Included the game file that holds the new booleans that determine rotatability.
    Also added the tetrimino file that defines these rules.

--- New folder/Tetrimino.cs
+++ New folder/Tetrimino.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tetrimino : MonoBehaviour { 
+public class Tetrimino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+    public bool allowRotation = true;
+    public bool limitRotation = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -14,22 +19,99 @@ public class Tetrimino : MonoBehaviour {
     {
         checkUserInput();
     }
-    
+
 
     //Constantly gathers User Input. Acts upon arrow-key input.
     void checkUserInput() {
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             transform.position += Vector3.right;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.left;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             transform.position += Vector3.left;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.right;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || (Time.time - fall >= fallSpeed))
+        {
             transform.position += Vector3.down;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.up;
+            }
+            fall = Time.time;
+
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.position += Vector3.up;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (allowRotation) {
+                if (limitRotation) {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+
+                else {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            
+            if (isValidPosition()) {
+
+            }
+            else {
+                if (limitRotation) {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else {
+                    transform.Rotate(0, 0, -90);
+                }
+
+            }
         }
+    }
+
+    bool isValidPosition() {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (FindObjectOfType<Game>().inGrid(pos) == false)
+            {
+                return false;
+            }
+
+        }
+        return true;
     }
 }
