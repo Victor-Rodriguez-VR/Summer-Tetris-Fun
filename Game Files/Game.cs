ec0cd14f6e8f90c86b937c853a3515d97b03beb9
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-09 21:12:26 -0700

    Tetriminos spawns + more file consistency.
    The tetriminos now spawn on the grid. Originally had errors where if tetriminos spawned, they'd duplicate. 
    Also renamed "new file" to "Game Files" to make traversing the Github more tolerable :)

---
+++ Game Files/Game.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{   //classic tetris games always have width set to 10, and height set to 20.
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNextTetrimino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public bool inGrid(Vector2 pos) {
+        //confirms tetrimino is within bounds of the x-plane axis, and the y plane axis.
+    return ((int)pos.x >=0 && (int)pos.x< gridWidth && (int)pos.y >= 0)    ;
+    }
+
+    //Helper method, helps rounds :)
+    public Vector2 Round(Vector2 pos) {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    //Instantiates a prefab.
+    public void spawnNextTetrimino() {
+        GameObject nextTetrimino =  (GameObject)Instantiate(Resources.Load(getRandomTetrimino(), typeof(GameObject)), new Vector2(5.0f,20.0f),  Quaternion.identity);
+    }
+
+    string getRandomTetrimino() {
+        int randomTetrimino = Random.Range(1, 8); // 1 to 7
+        string randomTetriminoName = "Prefabs/Tetris_L";
+        switch (randomTetrimino) {
+            case 1:
+                randomTetriminoName = "Prefabs/Tetris_L";
+                break;
+            case 2:
+                randomTetriminoName = "Prefabs/Tetris_Long";
+                break;
+            case 3:
+                randomTetriminoName = "Prefabs/Tetris_J";
+                break;
+            case 4:
+                randomTetriminoName = "Prefabs/Tetris_Square";
+                break;
+            case 5:
+                randomTetriminoName = "Prefabs/Tetris_S";
+                break;
+            case 6:
+                randomTetriminoName = "Prefabs/Tetris_T";
+                break;
+            case 7:
+                randomTetriminoName = "Prefabs/Tetris_Z";
+                break;
+        }
+        return randomTetriminoName;
+    }
+}
---
+++ Game Files/Tetrimino.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetrimino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkUserInput();
+    }
+
+
+    //Constantly gathers User Input. Acts upon arrow-key input.
+    void checkUserInput() {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.left;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.right;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || (Time.time - fall >= fallSpeed))
+        {
+            transform.position += Vector3.down;
+            if (isValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.up;
+            }
+            fall = Time.time;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (allowRotation) {
+                if (limitRotation) {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+
+                else {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            
+            if (isValidPosition()) {
+
+            }
+            else {
+                if (limitRotation) {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else {
+                    transform.Rotate(0, 0, -90);
+                }
+
+            }
+        }
+    }
+
+    bool isValidPosition() {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (FindObjectOfType<Game>().inGrid(pos) == false)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
