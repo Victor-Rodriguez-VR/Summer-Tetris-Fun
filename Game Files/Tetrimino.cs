0b2fda05fe10d6d36a87380efed043827b9443f7
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-09 21:43:13 -0700

    New spawns!
    One a tetrimino reaches the bottom of the grid, a new tetrimino block will spawn in :)

--- Game Files/Tetrimino.cs
+++ Game Files/Tetrimino.cs
@@ -58,6 +58,8 @@ public class Tetrimino : MonoBehaviour
             else
             {
                 transform.position += Vector3.up;
+                enabled = false;
+                FindObjectOfType<Game>().spawnNextTetrimino();
             }
             fall = Time.time;
 
